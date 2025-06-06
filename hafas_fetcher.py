5b1d4608334594130acb3e8a01f0391073820fbb
Franziska Kunsmann <hi@kunsmann.eu>
2025-06-06 19:47:42 +0200

    HAFASFetcher: log if no data was returned
    RMV does this if a stop is temporarily out of order, apparently

--- hafas_fetcher.py
+++ hafas_fetcher.py
@@ -77,9 +77,17 @@ class HAFASFetcher:
 
             payload = self._fetch_url(stop_id, url)
             if not self.data_sources == "arrivals":
-                data["Departure"] = payload["Departure"]
+                if "Departure" in payload:
+                    data["Departure"] = payload["Departure"]
+                else:
+                    log("Stop {stop_id} did not return any departures!".format(stop_id))
+                    data["Departure"] = []
             if not self.data_sources == "departures":
-                data["Arrival"] = payload["Arrival"]
+                if "Arrival" in payload:
+                    data["Arrival"] = payload["Arrival"]
+                else:
+                    log("Stop {stop_id} did not return any arrivals!".format(stop_id))
+                    data["Arrival"] = []
         else:
             url = lambda ep: API_MAPPING[CONFIG["api_provider"]].format(
                 endpoint=ep,
@@ -90,10 +98,18 @@ class HAFASFetcher:
 
             if not self.data_sources == "arrivals":
                 payload = self._fetch_url(stop_id, url("departureBoard"))
-                data["Departure"] = payload["Departure"]
+                if "Departure" in payload:
+                    data["Departure"] = payload["Departure"]
+                else:
+                    log("Stop {stop_id} did not return any departures!".format(stop_id))
+                    data["Departure"] = []
             if not self.data_sources == "departures":
                 payload = self._fetch_url(stop_id, url("arrivalBoard"))
-                data["Arrival"] = payload["Arrival"]
+                if "Arrival" in payload:
+                    data["Arrival"] = payload["Arrival"]
+                else:
+                    log("Stop {stop_id} did not return any arrivals!".format(stop_id))
+                    data["Arrival"] = []
 
         return data
 
