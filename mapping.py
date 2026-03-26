ee265f08797d4ab9affbb19fa80e9acd7fe2e468
Franziska Kunsmann <hi@kunsmann.eu>
2026-03-26 16:50:40 +0100

    add SaarVV

--- mapping.py
+++ mapping.py
@@ -1,14 +1,17 @@
 from mapping_rmv import MAPPING_RMV
+from mapping_saarvv import MAPPING_SAARVV
 from mapping_vbb import MAPPING_VBB
 
 COLOUR_MAPPING = {
     "rmv": MAPPING_RMV,
+    "saarvv": MAPPING_SAARVV,
     "vbb": MAPPING_VBB,
     "vbb-test": MAPPING_VBB,
 }
 
 API_MAPPING = {
     "rmv": "https://www.rmv.de/hapi/{endpoint}?id={stop}&duration={minutes}&format=json&accessId={key}",
+    "saarvv": "https://www.saarfahrplan.de/restproxy/{endpoint}?id={stop}&duration={minutes}&format=json&accessId={key}",
     "vbb": "https://fahrinfo.vbb.de/restproxy/2.32/{endpoint}?id={stop}&duration={minutes}&format=json&accessId={key}",
     "vbb-test": "https://vbb-demo.demo2.hafas.cloud/api/fahrinfo/latest/{endpoint}?id={stop}&duration={minutes}&format=json&accessId={key}",
 }
@@ -23,6 +26,13 @@ CATEGORY_MAPPING = {
         "5": "tram",
         "6": "bus",
     },
+    "saarvv": {
+        "0": "high_speed_rail",
+        "2": "bus",
+        "3": "low_speed_rail",
+        "6": "s_bahn",
+        "7": "bus",
+    },
     "vbb": {
         "0": "s_bahn",
         "1": "u_bahn",
@@ -36,6 +46,9 @@ CATEGORY_MAPPING["vbb-test"] = CATEGORY_MAPPING["vbb"]
 
 OPERATOR_LABEL_MAPPING = {
     "rmv": {
+        "^(Bus )": "",
+    },
+    "saarvv": {
         "^(Bus )": "",
     },
 }
